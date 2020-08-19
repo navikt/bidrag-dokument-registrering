6c86c59b5dc5ded5a817b5b17347a9e40ebc09f9
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-08-19 15:11:36 +0200

    bidrag-dokument-registrering er en java spring-boot app

---
+++ src/main/java/no/nav/bidrag/dokument/registrering/BidragDokumentRegistreringApplication.java
@@ -0,0 +1,13 @@
+package no.nav.bidrag.dokument.registrering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidragDokumentRegistreringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidragDokumentRegistreringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/nav/bidrag/dokument/registrering/BidragDokumentRegistreringApplicationTests.java
@@ -0,0 +1,13 @@
+package no.nav.bidrag.dokument.registrering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidragDokumentRegistreringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
